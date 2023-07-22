626e0c20384e4de9b0ee05d99aa27e90f4b447a8
iamafasha <afashaisakiye@gmail.com>
2023-07-22 16:42:29 +0300

    test 2

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,4 +6,34 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+//    public void setTimestamp(Date timestamp) {
+//        this.timestamp = timestamp;
+//    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
 }
